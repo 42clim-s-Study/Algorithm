9989e809226180601b5b125dda19c0c4b5e5b96f
Jiwon Woo <jwoo@c5r3s1.42seoul.kr>
2021-05-31 14:37:11 +0900

    jwoo_1967

--- 9week_1967_5639/jwoo_1967.py
+++ 9week_1967_5639/jwoo_1967.py
@@ -8,15 +8,6 @@ for _ in range(n - 1):
 	tree[child][0][0], tree[child][0][1] = parent, weight
 	tree[parent].append([child, weight])
 
-# def is_parent(stack_top, path_top):
-# 	global path
-# 	# for i in range(1, len(path[path_top])):
-# 	# 	child = path[path_top][i][0]
-# 	# 	if stack_top == child:
-# 	# 		return (1)
-# 	if path_top == path[stack_top][0][0]:
-# 	return (0)
-
 
 stack = [[1, 0]]
 copy_tree = copy.deepcopy(tree)
@@ -42,51 +33,47 @@ start_node = []
 for factor in last_node:
 	if factor[0] == max_weight:
 		start_node.append(factor[1])
-# print(start_node)
 
 def	is_in_path(path, stack_top):
+	if len(path) == 0:
+		return 0
 	for factor in path:
 		if stack and factor[0] == stack_top[0]:
-			# print("hihi", path, stack)
 			return 1
-	# print("hihi", path, stack)
 	return 0
 
 def	is_connect(path, stack_top):
 	global tree
 	for factor in tree[stack_top]:
 		if factor[0] == path[-1][0]:
-			# print("hihi", path, stack)
 			return 1
-	# print("hihi", path, stack)
 	return 0
 
-stack = [[start_node[0], 0]]
-copy_tree = copy.deepcopy(tree)
-total_weight = 0
 answer = 0
-# last_node = []
-visit = [0 for _ in range(n + 1)]
-path = []
-while stack:
-	# print(path, stack)
-	temp = stack.pop()
-	if is_in_path(path, temp) == 0:
-		path.append(temp)
-		total_weight += path[-1][1]
-	if len(copy_tree[path[-1][0]]) == 0:
-		# last_node.append([total_weight, path[-1][0]])
-		if len(tree[path[-1][0]]) == 1 and path[-1][0] != 0:
-			answer = max(answer, total_weight)
-			# print(path, total_weight)
-		while path and stack and is_connect(path, stack[-1][0]) == 0:
-			total_weight -= path.pop()[1]
-	else:
-		while len(copy_tree[path[-1][0]]) > 0:
-			stack.append(copy_tree[path[-1][0]].pop())
+for start in start_node:
+	stack = [[start, 0]]
+	copy_tree = copy.deepcopy(tree)
+	total_weight = 0
+	path = []
+	while stack:
+		temp = stack.pop()
+		if is_in_path(path, temp) == 0:
+			path.append(temp)
+			total_weight += path[-1][1]
+		if len(copy_tree[path[-1][0]]) == 0:
+			if path[-1][0] != start and len(tree[path[-1][0]]) == 1:
+				answer = max(answer, total_weight)
+				# print(path, total_weight)
+			while path and stack and is_connect(path, stack[-1][0]) == 0:
+				total_weight -= path.pop()[1]
+		else:
+			while len(copy_tree[path[-1][0]]) > 0:
+				stack.append(copy_tree[path[-1][0]].pop())
 
 print(answer)
 
+
+# 메모리 초과
 
 # import sys
 
